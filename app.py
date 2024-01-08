cf2cf392f44ee3f222c0bfa56d3ef97a27b6ade1
tamachika <62124334+tamachika@users.noreply.github.com>
2024-01-08 15:58:33 +0900

    Update app.pyブラウザ

--- app.py
+++ app.py
@@ -67,6 +67,10 @@ def handle_message(event):
                 break
         line_bot_api.reply_message(event.reply_token,
                                TextSendMessage(text=f"{event.message.text}は{ans}"))
+    else:
+        reply_message = "ビッグスプーン" #chat_completion(event.message.text)   
+        line_bot_api.reply_message(event.reply_token,
+                            TextSendMessage(text=reply_message))
     
 
 if __name__ == "__main__":
